5a220d28623f4ec78f5937bbdbdf394ff22b7bc7
Aries0331 <jinzhu@ualberta.ca>
2016-10-10 01:40:15 -0600

    initial

---
+++ Uber-like/app/src/main/java/com/example/uber_like/RiderActivity.java
@@ -0,0 +1,13 @@
+package com.example.uber_like;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider);
+    }
+}
