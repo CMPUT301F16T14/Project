1158b69a0ab0e68138b2af32fb0b87e6f6215360
Linkai Qi <linkai@ualberta.ca>
2016-10-14 15:09:06 -0600

    add Location constructor

--- app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/Location.java
+++ app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/Location.java
@@ -4,4 +4,6 @@ package ca.ualberta.project.cmput301f16t14.beep;
  * Created by administrator on 2016-10-13.
  */
 public class Location {
+    public Location(int x, int y) {
+    }
 }
--- app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/RequestList.java
+++ app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/RequestList.java
@@ -1,7 +1,23 @@
 package ca.ualberta.project.cmput301f16t14.beep;
 
+import java.util.ArrayList;
+
 /**
  * Created by administrator on 2016-10-13.
  */
 public class RequestList {
+    public ArrayList<Request> getRequest(UserAccount rider) {
+    }
+
+    public void add(Request myRequest) {
+    }
+
+    public void pushToServer() {
+    }
+
+    public ArrayList<Request> getByDriver(UserAccount driver) {
+    }
+
+    public ArrayList<Request> getAvailableRequest() {
+    }
 }
