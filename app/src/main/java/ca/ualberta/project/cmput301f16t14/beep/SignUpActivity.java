ef1e125e9d1036c41941f75d0d82c3aec1c12cb7
Ting Wang <ting8@cs.ualberta.ca>
2016-11-10 14:48:46 -0700

    make change to SignUpActivity

--- app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/SignUpActivity.java
+++ app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/SignUpActivity.java
@@ -1,7 +1,10 @@
 package ca.ualberta.project.cmput301f16t14.beep;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -11,6 +14,7 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText userName;
     private EditText userPhone;
     private EditText userEmail;
+    private Button finishButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,4 +28,25 @@ public class SignUpActivity extends AppCompatActivity {
 
         userName = (EditText) findViewById(R.id.NameEditText);
         userPhone = (EditText) findViewById(R.id.PhoneEditText);
+        userEmail = (EditText) findViewById(R.id.E_mailEditText);
+        finishButton = (Button) findViewById(R.id.finishSignUpbutton);
+
+        //reach here when user click the finish button
+        finishButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                String name = userName.getText().toString();
+                Integer phone = Integer.parseInt(userPhone.getText().toString());;
+                String email = userEmail.getText().toString();
+
+                //make a new profile with phone and email
+                Profile newProfile = new Profile(phone, email);
+
+                //destroy this page, return to last page
+                finish();
+
+            }
+        });
+    }
 }
