5abb5b8ceee6314ef90ef3a3d412b7574e21357c
Aries0331 <jinzhu@ualberta.ca>
2016-11-10 21:32:37 -0700

    update status bar

--- app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/DriverMainActivity.java
+++ app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/DriverMainActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class DriverMainActivity extends Activity {
+public class DriverMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/ViewProfileActivity.java
+++ app/src/main/java/ca/ualberta/project/cmput301f16t14/beep/ViewProfileActivity.java
@@ -2,6 +2,8 @@ package ca.ualberta.project.cmput301f16t14.beep;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +14,28 @@ public class ViewProfileActivity extends AppCompatActivity {
     private TextView currentUserPhone;
     private TextView currentUserEmail;
     private Button finishButton;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
