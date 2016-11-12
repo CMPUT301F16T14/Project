c5cb3da6cc278059ea3716be15b80b260ab8e8e8
301githublinkai <linkai@ualberta.ca>
2016-11-12 16:58:55 -0700

    Add relative layout to map activity

--- app/src/main/java/ca/ualberta/cs/linkai/beep/WelcomeActivity.java
+++ app/src/main/java/ca/ualberta/cs/linkai/beep/WelcomeActivity.java
@@ -39,18 +39,18 @@ public class WelcomeActivity extends Activity {
         username = usernameEditText.getText().toString();
 
         ElasticsearchAccountController.GetAccountTask getAccountTask = new ElasticsearchAccountController.GetAccountTask();
-        getAccountTask.execute(username);
+        getAccountTask.execute(username.toLowerCase());
 
         try {
             resultAccounts = getAccountTask.get();
         }
         catch (Exception e) {
-            Log.i("Error", "Failed to get the tweets out of the async object.");
+            Log.i("Error", "Failed to get the Accounts out of the async object.");
             Toast.makeText(WelcomeActivity.this, "Unable to find the Account on elastic search", Toast.LENGTH_SHORT).show();
         }
 
         if(resultAccounts.isEmpty()){
-            Toast.makeText(WelcomeActivity.this, "no results", Toast.LENGTH_SHORT).show();
+            Toast.makeText(WelcomeActivity.this, "Username not found", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(WelcomeActivity.this, resultAccounts.get(0).getEmail(), Toast.LENGTH_SHORT).show();
         }
