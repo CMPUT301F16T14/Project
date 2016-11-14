ce5e1cb8b3346995b9b893efa5510487ec06812f
Aries0331 <jinzhu@ualberta.ca>
2016-11-13 21:36:15 -0700

    add empty requestActivity

---
+++ app/src/main/java/ca/ualberta/cs/linkai/beep/RequestActivity.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.linkai.beep;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+    }
+}
