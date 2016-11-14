a877d40ef5da381f1fc386936d96ebbb9b500449
301githubting8 <ting8@ualberta.ca>
2016-11-14 15:38:42 -0700

    add location profile searching unit test and put HTML to doc folder

---
+++ app/src/test/java/ca/ualberta/cs/linkai/beep/ProfileTest.java
@@ -0,0 +1,149 @@
+package ca.ualberta.cs.linkai.beep;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by ting8 on 2016/11/14.
+ */
+
+public class ProfileTest {
+    // rider1
+    Account rider1 = new Account("testRiderName1", "7807101111", "CMPUT301@ualberta.ca");
+    // rider2 (with the same name as rider1)
+    Account rider2 = new Account("testRiderName1", "7807101331", "CMPUT321@ualberta.ca");
+    /**
+     * Test for UC-P01 (US03.01.01)
+     */
+    public void testCreateProfile(){
+        ArrayList<Account> resultAccounts = new ArrayList<Account>();
+        Account resultAccount;
+        String resultName;
+        String resultPhone;
+        String resultEmail;
+
+        // add the account to the elastic search server
+        ElasticsearchAccountController.AddAccountTask addAccountTask =
+                new ElasticsearchAccountController.AddAccountTask();
+        addAccountTask.execute(rider1);
+
+        // test if the profile is created
+        ElasticsearchAccountController.GetAccountTask getAccountTask =
+                new ElasticsearchAccountController.GetAccountTask();
+        getAccountTask.execute("testRiderName1");
+        try {
+            resultAccounts = getAccountTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get accounts from the elastic search", false);
+        }
+        resultAccount = resultAccounts.get(0);
+        resultName = resultAccount.getUsername();
+        assertTrue("name should be the same", resultName.equals("testRiderName1"));
+        resultPhone = resultAccount.getPhone();
+        assertTrue("phone should be the same", resultPhone.equals("7807101111"));
+        resultEmail = resultAccount.getEmail();
+        assertTrue("email should be the same", resultEmail.equals("CMPUT301@ualberta.ca"));
+
+        // create a user with a same username
+        // add the account to the elastic search server
+        addAccountTask.execute(rider2);
+
+        // test if the profile is created
+        getAccountTask.execute("testRiderName1");
+        // check the if the username has been registered already
+        getAccountTask.execute("testRiderName1");
+        try {
+            resultAccounts = getAccountTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get accounts from the elastic search", false);
+        }
+        assertTrue("the user with a same username shouldn't be added to the database",
+                resultAccounts.size() == 2);
+    }
+
+    /**
+     * Test for UC-P02 (US03.02.01)
+     */
+    public void testEditProfile(){
+        ArrayList<Account> resultAccounts = new ArrayList<Account>();
+        Account resultAccount;
+        // add the account to the elastic search server
+        ElasticsearchAccountController.AddAccountTask addAccountTask =
+                new ElasticsearchAccountController.AddAccountTask();
+        addAccountTask.execute(rider1);
+
+        // test if the profile is created
+        ElasticsearchAccountController.GetAccountTask getAccountTask =
+                new ElasticsearchAccountController.GetAccountTask();
+        getAccountTask.execute("testRiderName1");
+        try {
+            resultAccounts = getAccountTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get accounts from the elastic search", false);
+        }
+        resultAccount = resultAccounts.get(0);
+
+        // edit profile
+        resultAccount.setPhone("7807108888");
+        resultAccount.setEmail("TESTEMAIL@ualberta.ca");
+
+        // add the account to the elastic search server again
+        addAccountTask.execute(rider1);
+        //get it again
+        getAccountTask.execute("testRiderName1");
+        try {
+            resultAccounts = getAccountTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get accounts from the elastic search", false);
+        }
+        resultAccount = resultAccounts.get(0);
+
+
+        // test if the profile is edited
+        String phoneNumber = resultAccount.getPhone();
+        String email = resultAccount.getEmail();
+        assertTrue("phone number doesn't edited correctly", phoneNumber.equals("7807108888"));
+        assertTrue("email doesn't edited correctly", email.equals("TESTEMAIL@ualberta.ca"));
+    }
+
+    /**
+     * Test for UC-P03 (US03.03.01)
+     */
+    public void testViewProfile(){
+        ArrayList<Account> resultAccounts = new ArrayList<Account>();
+        Account resultAccount;
+        // add the account to the elastic search server
+        ElasticsearchAccountController.AddAccountTask addAccountTask =
+                new ElasticsearchAccountController.AddAccountTask();
+        addAccountTask.execute(rider1);
+
+        // test if the profile is created
+        ElasticsearchAccountController.GetAccountTask getAccountTask =
+                new ElasticsearchAccountController.GetAccountTask();
+        getAccountTask.execute("testRiderName1");
+        try {
+            resultAccounts = getAccountTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get accounts from the elastic search", false);
+        }
+        resultAccount = resultAccounts.get(0);
+
+        // test if the system can get the profile information
+        String username = resultAccount.getUsername();
+        String phoneNumber = resultAccount.getPhone();
+        String email = resultAccount.getEmail();
+
+        assertTrue("username doesn't display correctly", username.equals("testUserName"));
+        assertTrue("phone number doesn't display correctly", phoneNumber.equals("7807109999"));
+        assertTrue("email doesn't edited display", email.equals("CMPUT301@ualberta.ca"));
+    }
+}
---
+++ app/src/test/java/ca/ualberta/cs/linkai/beep/SearchingTest.java
@@ -0,0 +1,100 @@
+package ca.ualberta.cs.linkai.beep;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by ting8 on 2016/11/14.
+ */
+
+public class SearchingTest {
+
+    // rider1
+    Account rider1 = new Account("testRiderName1", "7807101111", "CMPUT301@ualberta.ca");
+    LatLng startLocation1 = new LatLng(-100, 200);
+    LatLng endLocation1 = new LatLng(-41, 139);
+
+    // rider2
+    Account rider2 = new Account("testRiderName2", "7807102222", "CMPUT302@ualberta.ca");
+    LatLng startLocation2 = new LatLng(-150, 250);
+    LatLng endLocation2 = new LatLng(-21, 94);
+
+    // driver
+    Account driver = new Account("testDriverName", "7807103333", "CMPUT303@ualberta.ca");
+
+    /**
+     * Test for UC-SE01 (US04.01.01 & US04.02.01)
+     * Test for UC-SE01-A (US04.01.01)
+     */
+    public void testSearchRequestsByGeo(){
+        ArrayList<Request> searchResult = new ArrayList<Request>();
+
+        // rider1 creates a request
+        Request testRequest1 = new Request(rider1, startLocation1, endLocation1);
+        testRequest1.setReason("Reason: for test!");
+
+        // rider2 creates a request
+        Request testRequest2 = new Request(rider2, startLocation2, endLocation2);
+
+        // add the request to the elastic search server
+        ElasticsearchRequestController.AddRequestTask addRequestTask =
+                new ElasticsearchRequestController.AddRequestTask();
+        addRequestTask.execute(testRequest1);
+        addRequestTask.execute(testRequest2);
+
+        LatLng testDriverLocation = new LatLng(-125, 225);
+        // TODO: not implement yet
+        ElasticsearchRequestController.GetRequestByGeoTask getRequestByGeoTask =
+                new ElasticsearchRequestController.GetRequestByGeoTask();
+        getRequestByGeoTask.execute(testDriverLocation);
+
+        try {
+            searchResult = getRequestByGeoTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get requests from the elastic search", false);
+        }
+        assertEquals(searchResult.size(), 2 );
+    }
+
+    /**
+     * Test for UC-SE01 (US04.01.01 & US04.02.01)
+     * Test for UC-SE01-B (US04.02.01)
+     */
+    public void testSearchRequestsByKeyword(){
+        ArrayList<Request> searchResult = new ArrayList<Request>();
+
+        // rider1 creates a request
+        Request testRequest1 = new Request(rider1, startLocation1, endLocation1);
+        testRequest1.setReason("Reason: for test!");
+
+        // rider2 creates a request
+        Request testRequest2 = new Request(rider2, startLocation2, endLocation2);
+        testRequest2.setReason("Reason: for test!");
+
+        // add the request to the elastic search server
+        ElasticsearchRequestController.AddRequestTask addRequestTask =
+                new ElasticsearchRequestController.AddRequestTask();
+        addRequestTask.execute(testRequest1);
+        addRequestTask.execute(testRequest2);
+
+        String testReason = new String("Reason: for test!");
+
+        // TODO: not implement yet
+        ElasticsearchRequestController.GetRequestByKeywordTask getRequestByKeywordTask =
+                new ElasticsearchRequestController.GetRequestByKeywordTask();
+        getRequestByKeywordTask.execute(testReason);
+
+        try {
+            searchResult = getRequestByKeywordTask.get();
+        }
+        catch (Exception e) {
+            assertTrue("Cannot get requests from the elastic search", false);
+        }
+        assertEquals(searchResult.size(), 2 );
+    }
+}
