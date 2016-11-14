41e5f0bc8f3584361a63676375ed12966cc3017d
Aries0331 <jinzhu@ualberta.ca>
2016-11-14 00:57:21 -0700

    enable phone call

---
+++ app/src/main/java/ca/ualberta/cs/linkai/beep/ViewProfileActivity.java
@@ -0,0 +1,103 @@
+package ca.ualberta.cs.linkai.beep;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static ca.ualberta.cs.linkai.beep.R.id.phoneTextView;
+import static ca.ualberta.cs.linkai.beep.R.id.textView;
+
+public class ViewProfileActivity extends Activity {
+
+    private TextView currentUserName;
+    private TextView currentUserPhone;
+    private TextView currentUserEmail;
+    private Button finishButton;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUserName = (TextView) findViewById(R.id.nameTextView);
+        currentUserPhone = (TextView) findViewById(R.id.phoneTextView);
+        currentUserEmail = (TextView) findViewById(R.id.emailTextView);
+        finishButton = (Button) findViewById(R.id.finishViewProfileButton);
+/*
+        //TODO: change text to current user profile!
+        currentUserName.setText("");
+        currentUserPhone.setText("");
+        currentUserEmail.setText("");
+*/
+
+        //http://stackoverflow.com/questions/8599657/dialing-a-phone-call-on-click-of-textview-in-android
+        // Make a call when click on phone number
+        //String phoneNumber = currentUserPhone.getText().toString();
+        currentUserPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentUserPhone.setText("123456");
+                String phoneNumber = "123456";
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:+" + phoneNumber.trim()));
+                if (ActivityCompat.checkSelfPermission(ViewProfileActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+
+            }
+        });
+/*
+        //reach here when user click the finish button
+        finishButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                //destroy this page, return to last page
+                finish();
+
+            }
+        });*/
+
+    }
+
+}
