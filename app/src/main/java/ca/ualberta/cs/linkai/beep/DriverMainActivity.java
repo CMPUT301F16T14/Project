c15b50b736cf4e53078265ccedcc86cc873d99af
Aries0331 <jinzhu@ualberta.ca>
2016-11-13 18:38:12 -0700

    add editprofile, hamburger button

--- app/src/main/java/ca/ualberta/cs/linkai/beep/DriverMainActivity.java
+++ app/src/main/java/ca/ualberta/cs/linkai/beep/DriverMainActivity.java
@@ -1,7 +1,10 @@
 package ca.ualberta.cs.linkai.beep;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DriverMainActivity extends Activity {
 
@@ -9,5 +12,37 @@ public class DriverMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_driver_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, EditProfileActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.view_request) {
+            Intent intent = new Intent(this, EditProfileActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
