f5c1d3759ad1fe7189a2fc251513a029734976d6
Aries0331 <jinzhu@ualberta.ca>
2016-11-26 13:14:52 -0700

    update keyword search page

--- app/src/main/java/ca/ualberta/cs/linkai/beep/SearchByKeywordActivity.java
+++ app/src/main/java/ca/ualberta/cs/linkai/beep/SearchByKeywordActivity.java
@@ -3,12 +3,23 @@ package ca.ualberta.cs.linkai.beep;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class SearchByKeywordActivity extends Activity {
+
+    EditText keyword;
+    Button search;
+    ListView resultList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_by_keyword);
+
+        keyword = (EditText) findViewById(R.id.keywordSearch);
+        search = (Button) findViewById(R.id.search);
+        resultList = (ListView) findViewById(R.id.resultList);
     }
 }
